b5e665e481cac965bb684e7d698c9bdb174feb29
shadrach i <itodoshadrach@gmail.com>
2022-12-20 14:16:20 +0100

    modified the database file

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    // const conn = await mongoose.connect(process.env.MONGO_URI);
-
-    const conn = await mongoose.connect("mongodb://localhost:27017/test");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`mongoDB connected: ${conn.connection.host}`);
   } catch (error) {
